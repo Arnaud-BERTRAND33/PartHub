90c1e663b8cfcc7448160112921779079f6a4dc7
Christophe Fondeville <fondevillechristophe@gmail.com>
2022-04-21 12:23:02 +0200

    bidouille

--- src/Controller/ConnexionController.php
+++ src/Controller/ConnexionController.php
@@ -12,7 +12,6 @@ class ConnexionController extends AbstractController
     public function inscription(): string
     {
         if ($_POST) {
-
             return $this->twig->render('Dashboard/dashboard.html.twig');
         }
         return $this->twig->render('Connexion/inscription.html.twig');
