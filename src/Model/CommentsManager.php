1d1d9d00d3d5ffc9f8d7a02a4f1d8e16d6fcbb6e
MaraLfs <99500975+MaraLfs@users.noreply.github.com>
2022-05-02 21:30:30 +0200

    modifs page commentaires

--- src/Model/CommentsManager.php
+++ src/Model/CommentsManager.php
@@ -10,21 +10,21 @@ class CommentsManager extends AbstractManager
     public function insert(array $comment): void
     {
             $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE .
-            " (`comment`, `date`, `user_id`, `event_id`)
-            VALUES (:comment, NOW(), :user_id, :event_id)");
+            " (`comment`, `date`, `user_id`, `event_id`, `picture` )
+            VALUES (:comment, NOW(), :user_id, :event_id, :picture)");
             $statement->bindValue('comment', $comment['comment']);
             $statement->bindValue('user_id', $comment['user_id']);
             $statement->bindValue('event_id', $comment['event_id']);
+        $statement->bindValue('picture', $comment['picture']);
 
             $statement->execute();
     }
 
     public function selectByPartyId($eventId): array
     {
-        $query = "SELECT c.comment, c.date, u.firstname, u.picture FROM comment c
+        $query = "SELECT c.comment, c.date, c.picture, u.firstname, u.picture FROM comment c
 JOIN user u ON c.user_id=u.id WHERE c.event_id=$eventId;";
 
         return $this->pdo->query($query)->fetchAll() ?: [];
     }
-
 }
