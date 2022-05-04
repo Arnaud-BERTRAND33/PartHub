8eb8a53a2f9470e3ae92e054c9f1e7b61425f428
ESCARMANT Adrien <92037454+E-Adrien8@users.noreply.github.com>
2022-05-04 21:58:56 +0200

    progression

--- src/Controller/GuestsController.php
+++ src/Controller/GuestsController.php
@@ -18,12 +18,40 @@ class GuestsController extends AbstractController
 
     public function guests(int $partyId): string
     {
+
         $guestManager = new GuestsManager();
-        $guests = $guestManager->selectGuests();
+        $guests = $guestManager->selectGuests($partyId);
 
         return $this->twig->render('Guests/guests.html.twig', [
-             "guests" => $guests,
+            "guests" => $guests,
             'party_id' => $partyId,
         ]);
     }
+
+    public function participate(): string
+    {
+        $participateManager = new GuestsManager();
+        $participates = $participateManager->selectGuests();
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $participate = array_map($_POST);
+
+            $participate['party_id'] = $_GET['party_id'];
+            $participate['user_id'] = $this->user['id'];
+
+            $participatesManager = new GuestsManager();
+            $participatesManager->insert($user_has_party);
+            header('Location: /party/comments?party_id=' . $partyId);
+            return '';
+        }
+        return $this->twig->render('Guests/guests.html.twig', [
+            'participates' => $participates,
+        ]);
+    }
+//            return $this->twig->render('Guests/guests.html.twig', [
+//                'participates' => $participates,
+//                'party_id' => $partyId,
+//        ]);
 }
+
+
--- src/Model/GuestsManager.php
+++ src/Model/GuestsManager.php
@@ -7,12 +7,26 @@ class GuestsManager extends AbstractManager
     public const TABLE = 'user';
     public const TABLE2 = 'user_has_party';
 
-    public function selectGuests(): array
+//recuperation tableau participant
+    public function selectGuests(int $partyId): array
     {
-        $query = 'SELECT u.picture, u.firstname, u.lastname, u.telephone, u.email, h.participate
+        $query = "SELECT u.picture, u.firstname, u.lastname, u.telephone, u.email, h.participate
                FROM user u
                   JOIN user_has_party h ON u.id=h.user_id
-                 WHERE h.party_id=1;';
+                 WHERE h.party_id=$partyId;";
         return $this->pdo->query($query)->fetchAll();
+    }
+//insertion si participe, peut etre ou pas
+    public function insert(array $user_has_party): void
+    {
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE2 . "
+        (`user_id`, `party_id`, `participate`)
+        VALUES (:user_id, :party_id, :participate)");
+
+        $statement->bindValue('user_id', $user_has_party['user_id']);
+        $statement->bindValue('party_id', $user_has_party['party_id']);
+        $statement->bindValue('participate', $user_has_party['participate']);
+
+        $statement->execute();
     }
 }
